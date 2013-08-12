908e4841e37769a8f3feeac89cd46d90903524de
pradeep <murugesan.pradeep@gmail.com>
2013-08-12 15:25:14 +0530

    cahnged config and added the css

--- app.js
+++ app.js
@@ -9,9 +9,25 @@ var express = require('express')
   , rentals = require('./routes/rentals')
   , http = require('http')
   , path = require('path')
-  , templatizer = require('templatizer');
+  , templatizer = require('templatizer')
+  , mysql =  require('mysql');
+
 
 var app = express();
+var devDb = {
+    host:'localhost',
+    port: '3306',
+    user: 'rentalUser',
+    password:'rentalUser',
+    database: 'Rentals'
+};
+var prodDb = {
+    host:'localhost',
+    port: '10000',
+    user: 'uJUDtGchPR36n',
+    password: 'pWSrnJZTzDXUH',
+    database: 'd9776c92981d14fdeaa7e62f927b207'
+};
 
 // all environments
 app.set('port', process.env.PORT || 8888);
@@ -23,6 +39,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('db', devDb);
 var jadeClientTemplatesDir = __dirname + "/views/clientTemplates" ;
 var jsTemplateDir = __dirname + "/public/javascripts/template";
 templatizer(jadeClientTemplatesDir, jsTemplateDir + '/template.js');
@@ -31,7 +48,13 @@ templatizer(jadeClientTemplatesDir, jsTemplateDir + '/template.js');
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
+if ('production' == app.get('env')) {
+  app.set('db', prodDb);
+}
+var config = app.get('db');
+var db = mysql.createConnection( config );
+//console.log(db);
+express.request.db = express.response.db = db;
 app.get('/', routes.index);
 app.get('/rentals', rentals.list);
 app.get('/rentals/:id', rentals.findById);
--- routes/rentals.js
+++ routes/rentals.js
@@ -1,35 +1,39 @@
 var mysql =  require('mysql');
-var db = mysql.createConnection({
-    host:'localhost',
-    port: '3306',
-    user: 'rentalUser',
-    password:'rentalUser',
-    database: 'Rentals'
-});
+var app = require("../app");
+var db = app.db;
+//var config = app.get('db');
+//var db = mysql.createConnection( config );
 
-db.connect(function(error) {
+/*db.connect(function(error) {
     if(error) {
         console.log("error while connecting with db.");
     }
-});
+    else {
+        var sql = "use " + config.name;
+        db.query(sql, function(error){
+            if(error) {
+                console.log("Coudn't use the DB " + config.name);
+            }
+        });
+    }
+});*/
 
 exports.list = function(req, res) {
-    var sql = "SELECT Id, ApartmentName, Location, cast(Visited AS UNSIGNED) AS Visited FROM Rentals.Rental;";
+    var sql = "SELECT Id, ApartmentName, Location, cast(Visited AS UNSIGNED) AS Visited FROM Rental;";
     var rentals = [];
-    db.query(sql, function(error, results) { 
+    req.db.query(sql, function(error, results) { 
         if(error) {
             res.send({"error" : true })
         }
         else {
-            console.log(results);
             res.send(results);
         }
     });
     //res.send(JSON.stringify(rentals));
 };
 exports.markVisited = function(req, res) {
-    var sql = "UPDATE Rentals.Rental SET Visited = true  where Rental.id = " + req.params.id ;
-    db.query(sql, function(error, results) {
+    var sql = "UPDATE Rental SET Visited = true  where Rental.id = " + req.params.id ;
+    req.db.query(sql, function(error, results) {
         if(error) {
             res.send({ "error" : true });
         }
@@ -39,8 +43,8 @@ exports.markVisited = function(req, res) {
     });
 };
 exports.findById = function(req, res) {
-    var sql = "SELECT * FROM Rentals.Rental where Rental.id = " + req.params.id ;
-    db.query(sql, function(error, results) {
+    var sql = "SELECT * FROM Rental where Rental.id = " + req.params.id ;
+    req.db.query(sql, function(error, results) {
         if(error) {
             res.send({ "error" : true });
         }
@@ -52,7 +56,7 @@ exports.findById = function(req, res) {
 
 exports.addRental = function(req, res) {
     rental = req.body;
-    var sql = "INSERT INTO `Rentals`.`Rental` (`ApartmentName`, `OwnerOrDealer`, `webpage`, `visited`, `RentAmount`, `Location`, `OwnerName`, `OwnerPhone`) VALUES ( '" +
+    var sql = "INSERT INTO `Rental` (`ApartmentName`, `OwnerOrDealer`, `webpage`, `visited`, `RentAmount`, `Location`, `OwnerName`, `OwnerPhone`) VALUES ( '" +
         rental.ApartmentName + "','" +
         rental.OwnerOrDealer+ "','" +
         rental.webpage + "'," +
@@ -61,7 +65,7 @@ exports.addRental = function(req, res) {
         rental.Location + "','" +
         rental.OwnerName + "','" +
         rental.OwnerPhone + "');" ;
-    db.query(sql, function(error) {
+    req.db.query(sql, function(error) {
         if(error) {
             res.send({"error": true});
         } else {
